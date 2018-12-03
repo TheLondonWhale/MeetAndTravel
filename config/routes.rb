2276457f86e236690ccfb9063212c0e785aace14
kirimari <ait.mus06@gmail.com>
2018-12-03 15:12:19 +0100

    Add Module Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+  omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+  get 'home/private'
+  root to: "home#index"
 end
