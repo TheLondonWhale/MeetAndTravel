506d81a3c53b4482232f20884541420f4a6524e5
TheLondonWhale <thomas.perus@espeme.com>
2018-12-04 15:31:06 +0100

    add admin to User. Create tables Tip, Category and Join_table

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,2 +1,4 @@
 class Tip < ApplicationRecord
+  has_many :category_to_tips
+  has_many :categories, through: :category_to_tips
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_03_172851) do
+ActiveRecord::Schema.define(version: 2018_12_04_135356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "category_to_tips", force: :cascade do |t|
+    t.bigint "tip_id"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_category_to_tips_on_category_id"
+    t.index ["tip_id"], name: "index_category_to_tips_on_tip_id"
+  end
 
   create_table "tips", force: :cascade do |t|
     t.string "title"
     t.string "metadata"
     t.text "description"
+    t.string "image"
     t.float "latitude"
     t.float "longitude"
-    t.string "image"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_tips_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "username", default: "", null: false
+    t.string "firstname"
+    t.string "lastname"
+    t.integer "age"
+    t.text "description"
+    t.string "image"
+    t.boolean "admin", default: false
+    t.float "longitude"
+    t.float "latitude"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -39,6 +60,7 @@ ActiveRecord::Schema.define(version: 2018_12_03_172851) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "facebook_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
