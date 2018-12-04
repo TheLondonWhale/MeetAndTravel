285f72c42bb72f74e3ddb7f69b64f75cdcbec769
kirimari <ait.mus06@gmail.com>
2018-12-04 12:21:32 +0100

    Fix Asset precompile issues

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module MeetTravel
     config.load_defaults 5.2
 
     config.action_mailer.delivery_method = :mailjet
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
