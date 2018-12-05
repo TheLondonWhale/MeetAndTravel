dfcb5398b4311e43791d46da95e1d64152f2e9e9
Anaïs <anais.diologeant@hotmail.fr>
2018-12-05 17:24:53 +0100

    front registration and edit profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_devise_parameters, if: :devise_controller?
 
   def configure_devise_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :image)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:firstname, :username, :email, :password, :password_confirmation, :image)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :firstname,
+      :lastname, :description, :birthdate, :password, :password_confirmation, :current_password, :image)}
   end
 
 end
