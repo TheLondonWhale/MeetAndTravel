27caeabf7ed73d12664fe974584daa4aa7286bac
kirimari <ait.mus06@gmail.com>
2018-12-11 14:27:53 +0100

    resole search fonction in index.html

--- app/models/search.rb
+++ app/models/search.rb
@@ -9,31 +9,25 @@ class Search < ApplicationRecord
 
     tips_by_keyword = Tip.where(["title LIKE?",keywords]) if keywords.present?
     tips_by_cat = Category.where(["id = :id",{id:category_id}]) if category_id.present?
-
     if tips_by_keyword != nil
       tips_by_keyword.each do |tip|
         tip.id
         table_of_ids_title << tip.id
       end
     end
-
     if tips_by_cat != nil
       Category.find(tips_by_cat[0].id).tips.each do |tip|
         table_of_ids_cats << tip.id
       end
     end
+merged_table_of_tips_ids = [table_of_ids_title, table_of_ids_cats].flatten
+merged_table_of_uniq_tips_ids = merged_table_of_tips_ids.uniq
 
-merged_table_of_tips_ids = table_of_ids_title & table_of_ids_cats
-
- binding.pry
-
- merged_table_of_tips_ids.each do |tip|
+ merged_table_of_uniq_tips_ids.each do |tip|
    result = Tip.find(tip)
-   binding.pry
    results << result
  end
- binding.pry
+
     return results
   end
-
 end
